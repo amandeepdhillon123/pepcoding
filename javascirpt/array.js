66f4f3a53b52f130383ee398594fb6f4a006c6ac
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-06 20:19:48 +0530

    array

---
+++ javascirpt/array.js
@@ -0,0 +1,43 @@
+
+// array 
+// collection of lements in jabvvascript 
+// can insert different datatype 
+
+let arr =[1,2,3,4];
+
+// console.log(arr);
+
+// index and length change element
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+
+arr[3]="ram"
+// console.log(arr);
+// console.log(arr.length);
+
+// push method added to last element of array
+
+arr.push("jai shrre ram ");
+// console.log(arr);
+
+// pop method deleted last elelemtn 
+
+arr.pop();
+console.log(arr);
+
+// unshift
+
+arr.unshift("12000");
+
+console.log(arr)
+
+arr.shift()
+console.log(arr);
+
+
+
+
+
