be40e72a01feb2a9e09cc7e58dcd7625337f2fec
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-09 09:27:42 +0530

    dir

---
+++ node/package/dir.js
@@ -0,0 +1,13 @@
+
+
+const fs = require('fs');
+
+// creation of director 
+
+// fs.mkdirSync('mydirectory')
+
+
+// delete directoryt 
+
+fs.rmdirSync('mydirectory');
+console.log('my directory deleted')
