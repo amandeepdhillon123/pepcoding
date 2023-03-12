7de07096948fe72c72c40f0ec88dc989f088e2bf
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-12 16:31:27 +0530

    array

---
+++ javascirpt/array.js
@@ -0,0 +1,33 @@
+
+
+let arr =[1,2,3,'pepcoding',12.5];
+console.log(arr);
+
+arr[3]="amandeep";
+
+console.log(arr);
+// length
+let length=arr.length;
+console.log(length);
+
+// push 
+
+arr.push("ram");
+
+console.log(arr)
+
+// pull
+
+arr.pop();
+console.log(arr)
+
+// unshift
+
+arr.unshift('12000')
+console.log(arr);
+
+// shift
+
+arr.shift();
+
+console.log(arr);
