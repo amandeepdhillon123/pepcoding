be34495b94d5b8441bc4d9bf87a4920e21c40f7c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-13 18:24:38 +0530

    strings concepts

--- javascirpt/strings.js
+++ javascirpt/strings.js
@@ -2,3 +2,16 @@ var str="my name is amandeep Dhillon";
 // console.log(str);
 
 // console.log(str.length);
+
+// slice (start, end+1)
+
+let slice= str.slice(2,7)
+// console.log(slice)
+
+// substr(start, length)
+
+let substr= str.substr(2,8)
+// console.log(substr)
+
+
+
